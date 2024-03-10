b0fc6d6574825223093459b895f899fbba11f50a
seung-heee <tmdgmldi2595@naver.com>
2024-03-10 13:51:52 +0900

    Feat: About

--- src/pages/user/About.jsx
+++ src/pages/user/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FaHandshakeSimple } from "react-icons/fa6";
 import { images } from '../../utils/images';
+import person from '../../utils/person.json';
 
 const About = () => {
   return (
-    <div>
+    <div className='bg-[#FFF4ED]'>
       <AboutTitle />
       <AboutPerson />
     </div>
@@ -48,11 +49,18 @@ export const AboutPerson = () => {
 export const PuzzlePerson = () => {
   return(
     <div className='my-4'>
-      <div className='w-10/12 mx-auto text-center'>
+      <div className='w-10/12 mx-auto text-center mb-5'>
         <img src={images.puzzle} alt="puzzle" className='w-2/12 mx-auto mb-1'/>
         <p className='text-xs'>성결대학교<br/>제40대 Puzzle 동아리연합회</p>
       </div>
-    <div></div>
+
+      <div className='grid grid-cols-3 text-white'>
+        {person.filter(person => person.group === "puzzle").map((person)=>{
+          return (
+            <Person person={person} />
+          )
+        })}
+      </div>
     </div>
   )
 }
@@ -60,11 +68,36 @@ export const PuzzlePerson = () => {
 export const LikelionPerson = () => {
   return(
     <div className='my-4'>
-      <div className='w-10/12 mx-auto text-center'>
+      <div className='w-10/12 mx-auto text-center mb-5'>
         <img src={images.likelion} alt="likelion" className='w-2/12 mx-auto mb-1'/>
         <p className='text-xs'>성결대학교<br/>멋쟁이사자처럼 12기</p>
       </div>
-      <div></div>
+
+      <div className='flex flex-wrap w-8/12 mx-auto text-white'>
+        {person.filter(person => person.group === "likelion").map((person)=>{
+          return (
+            <Person person={person} />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export const Person = ({person}) => {
+  return (
+    <div className='text-[10px] text-center py-3 bg-[#654F40] rounded-xl w-[110px] mx-auto mb-3'>
+      <div className='h-[75px] w-[75px] flex items-center mx-auto text-center'>
+        <img src={person.image} className='h-full mx-auto'/>
+      </div>
+      <div className='flex items-center justify-center p-1'>
+        <img src={person.logo} width={20}/>
+        <div className='ml-2'>{person.name}</div>
+      </div>
+      <div className={`p-[1px] ${person.department === '미디어소프트웨어학과' && 'text-[8px]'}`}>{person.department}</div>
+      <div className='p-1'>{person.position}</div>
+      <hr className='p-1 w-10/12 mx-auto'/>
+      <div className='text-[#FFCB67]'>{person.role}</div>
     </div>
   )
 }
