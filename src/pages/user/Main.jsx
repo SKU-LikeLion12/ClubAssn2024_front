68c0f27b0b3e5baa3a6ea1754542e119ec702e21
minseo121 <kittse1217@gmail.com>
2024-03-11 01:27:27 +0900

    fix : main a 태그

--- src/pages/user/Main.jsx
+++ src/pages/user/Main.jsx
@@ -12,15 +12,15 @@ const Main = () => {
         <div className='textFont text-white text-lg mt-20'>
           <div className='grid grid-rows-1 grid-flow-col justify-center relative mb-[-68px] z-10'>
             <img src="assets/images/puzzle/puzzleG.png" className='w-auto h-32 ml-16 mr-1'/>
-            <a className='absolute text-center top-8 left-[5.5rem]'>퍼즐 조각 <br/> 모으기</a>
+            <a href ="#" className='absolute text-center top-8 left-[5.5rem]'>퍼즐 조각 <br/> 모으기</a>
             <img src="assets/images/puzzle/puzzleY.png" className='w-32 h-auto mr-20 ml-1'/>
-            <a className='absolute text-center top-8 left-60'>물품<br/>대여</a>
+            <a href ="#" className='absolute text-center top-8 left-60'>물품<br/>대여</a>
           </div>
           <div className='grid grid-rows-1 grid-flow-col justify-center relative z-10'>
             <img src="assets/images/puzzle/puzzleR.png" className='w-32 h-auto ml-16 mb-20'/>
-            <a className='absolute text-center top-[4.5rem] left-[5.5rem]'>동아리<br/>미니맵</a>
+            <a href ="#" className='absolute text-center top-[4.5rem] left-[5.5rem]'>동아리<br/>미니맵</a>
             <img src="assets/images/puzzle/puzzleB.png" className='w-auto h-32 mr-36 ml-3 mt-[30px]'/>
-            <a className='absolute text-center top-[4.5rem] left-56'>강의실<br/>시간표</a>
+            <a href ="#" className='absolute text-center top-[4.5rem] left-56'>강의실<br/>시간표</a>
           </div>
           <div className='flex justify-start'>
           <img src="assets/images/puzzle/backPuzzleB.png" className='absolute z-[-10] w-2/5 h-auto top-[18rem]'/>
