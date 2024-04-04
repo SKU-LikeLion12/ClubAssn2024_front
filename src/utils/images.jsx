28f812c3f981922a6cd7bb0452df699e0afad412
ckdwns1221 <ckdwns2181@gmail.com>
2024-04-04 16:15:56 +0900

    rename: 이미지파일  구조 및 이름 변경

--- src/utils/images.jsx
+++ src/utils/images.jsx
@@ -1,11 +1,15 @@
 const PATH = process.env.PUBLIC_URL;
 
 export const images = {
-    // Background
-    leftPinkMenu: PATH + '/assets/images/puzzlePiece/leftPinkMenu.png',
-    rightPinkMenu: PATH + '/assets/images/puzzlePiece/rightPinkMenu.png',
-    leftGreen: PATH + '/assets/images/puzzlePiece/leftGreen.png',
-    rightGreen: PATH + '/assets/images/puzzlePiece/rightGreen.png',
+    // BgPuzzle
+    leftPinkMenu: PATH + '/assets/images/BgPuzzle/leftPinkPuzzle.png',
+    rightPinkMenu: PATH + '/assets/images/BgPuzzle/rightPinkPuzzle.png',
+    leftGreen: PATH + '/assets/images/BgPuzzle/leftGreenPuzzle.png',
+    rightGreen: PATH + '/assets/images/BgPuzzle/rightGreenPuzzle.png',
+    LYP: PATH + '/assets/images/BgPuzzle/LeftYellowPuzzle.svg',
+    RYP: PATH + '/assets/images/BgPuzzle/RightYellowPuzzle.svg',
+    backPuzzleT: PATH + '/assets/images/BgPuzzle/rightCoralPuzzle.svg',
+    backPuzzleB: PATH + '/assets/images/BgPuzzle/leftCoralPuzzle.svg',
     
     // PuzzlePiece
     menuPuzzle: PATH + '/assets/images/puzzlePiece/menuPuzzle.png',
@@ -15,20 +19,16 @@ export const images = {
     likelion: PATH + '/assets/images/logo/likelion.png',
 
     //퍼즐조각
-    puzzleG: PATH + '/assets/images/puzzle/puzzleG',
-    puzzleB: PATH + '/assets/images/puzzle/puzzleB',
-    puzzleR: PATH + '/assets/images/puzzle/puzzleR',
-    puzzleY: PATH + '/assets/images/puzzle/puzzleY',
+    puzzleG: PATH + '/assets/images/puzzlePiece/puzzleG',
+    puzzleB: PATH + '/assets/images/puzzlePiece/puzzleB',
+    puzzleR: PATH + '/assets/images/puzzlePiece/puzzleR',
+    puzzleY: PATH + '/assets/images/puzzlePiece/puzzleY',
 
     // SNS
     instagram: PATH + '/assets/images/sns/instagram.png',
     kakaotalk: PATH + '/assets/images/sns/kakaotalk.png',
     naverCafe: PATH + '/assets/images/sns/naverCafe.png',
     youtube: PATH + '/assets/images/sns/youtube.png',
-
-    // Rental
-    LYP: PATH + '/assets/images/rental/LeftYellowPuzzle.svg',
-    RYP: PATH + '/assets/images/rental/RightYellowPuzzle.svg',
 
     // map
     union3F: PATH + '/assets/images/map/studentUnion3F.png',
