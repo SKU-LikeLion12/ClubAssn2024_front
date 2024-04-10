68ad885e29aeb21bf7dfc978fb6f0108f22506b3
un-known0 <nam0221@naver.com>
2024-04-10 22:05:41 +0900

    [add] 조회수 체크 로직 추가, baseURL 변경

--- src/App.js
+++ src/App.js
@@ -5,8 +5,39 @@ import Admin from "./routes/Admin.jsx";
 import { LoginProvider } from "./context/LoginContext.jsx";
 import { useEffect } from "react";
 import ScrollToTop from "./components/shared/ScrollToTop.jsx";
+import { getCookie, setCookie } from './utils/cookie';
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+    const cookie = getCookie('visitor');
+    if (!cookie) {
+      const currentDate = new Date();
+      const expiresDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate() + 1, // 다음 날로 설정
+        0, // 시간을 00시로 설정
+        0, // 분을 00분으로 설정
+        0, // 초를 00초로 설정
+      );
+      // 시차 보정
+      setCookie('visitor', 'visitor', { path: './', expires: expiresDate, secure: true });
+      callAPI();
+    }
+  }, []);
+
+  const callAPI = () => {
+    axios.get('https://api.sku-sku.com/visitors/puzzle')
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('API Error: ', error);
+      })
+  }
+
   return (
     <LoginProvider>
       <div className="App textFont">
